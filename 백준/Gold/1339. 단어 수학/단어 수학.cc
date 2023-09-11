bcaea1672805b1d18ae844779ca7000f21d0b04f
ash9river <121378532+ash9river@users.noreply.github.com>
2023-09-11 13:52:29 +0900

    [Gold IV] Title: 단어 수학, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Gold/1339. 단어 수학/단어 수학.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <queue>
+#include <map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int time(int t){
+    if(t==0) return 1;
+    int mm=1;
+    for(int i=0;i<t;++i){
+        mm*=10;
+    }
+    return mm;
+}
+int main(){
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    int n;
+    cin>>n;
+    vector<pair<int,string>> v(n);
+    map<char,int> table;
+    map<char,int> m;
+    priority_queue<pair<int,char>> q;
+    string a;
+    int result=0;
+    int ptr=9;
+    for(int i=0;i<n;++i){
+        cin>>a;
+        v[i]={-a.size(),a};
+    }
+    sort(v.begin(),v.end());
+    for(int i=0;i<n;++i){
+        int tmpSize=v[i].second.size();
+        for(int j=0;j<tmpSize;++j){
+            if(table.count(v[i].second[j])>0){
+                int tmp=table[v[i].second[j]];
+                table[v[i].second[j]]=tmp+time(tmpSize-1-j);
+            }else{
+                table[v[i].second[j]]=time(tmpSize-1-j);
+            }
+        }
+    }
+    for(auto it=table.begin();it!=table.end();++it){
+        char key=it->first;
+        int value=it->second;
+        q.push({value,key});
+    }
+    while(!q.empty()){
+        char key=q.top().second;
+        int val=q.top().first;
+       /*  cout<<key<<" "<<val<<'\n'; */
+        q.pop();
+        m[key]=ptr--;
+        if(ptr<0) break;
+    }
+    for(int i=0;i<v.size();++i){
+        for(int j=0;j<v[i].second.size();++j){
+            int tmp=m[v[i].second[j]];
+            result+=tmp*time(v[i].second.size()-1-j);
+            /* cout<<result<<" "; */
+        }
+    }
+    cout<<result;
+}
