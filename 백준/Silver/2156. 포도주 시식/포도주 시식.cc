aa7dd7ee9e380f3c3925a8209e7e41e1db3b86e6
ash9river <121378532+ash9river@users.noreply.github.com>
2023-09-13 19:06:41 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2296 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -5,9 +5,9 @@
 using namespace std;
 int cache[10000][2];
 vector<int> v(10000);
-int dp[10000];
+int dp[10001];
 int n;
-int pathMax(int now,int pass){
+/* int pathMax(int now,int pass){
 	if(now>=n) return 0;
 	int& ret=cache[now][pass];
 	if(ret!=-1) return ret;
@@ -17,7 +17,7 @@ int pathMax(int now,int pass){
 	else{
 		return ret=max(v[now]+pathMax(now+1,pass+1),pathMax(now+1,0));
 	}
-}
+} */
 int main(){
 	cin.tie(0);
 	ios_base::sync_with_stdio(0);
@@ -25,6 +25,21 @@ int main(){
 	for(int i=0;i<n;++i){
 		cin>>v[i];
 	}
-	memset(cache,-1,sizeof(cache));
-	cout<<pathMax(0,0);
+	if(n==1){
+		cout<<v[0];
+		return 0;
+	}
+	else if(n==2){
+		cout<<v[0]+v[1];
+		return 0;
+	}
+	else{
+		dp[0]=v[0];
+		dp[1]=v[0]+v[1];
+		dp[2]=v[0]+v[1]+v[2]-min({v[0],v[1],v[2]});
+		for(int i=3;i<n;++i){
+			dp[i]=max(dp[i-1],max(dp[i-2],dp[i-3]+v[i-1])+v[i]);
+		}
+		cout<<dp[n-1];
+	}
 }
