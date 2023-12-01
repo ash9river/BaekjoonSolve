60fc813f0d473c612d6f68afb5a7eb414a83779c
ash9river <121378532+ash9river@users.noreply.github.com>
2023-12-01 14:36:06 +0900

    [Gold V] Title: 강의실, Time: 64 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define ll long long
+using namespace std; //21퍼틀
+int main(){
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	int n;
+	cin>>n;
+	ll a,b,c;
+	ll maxGae=0,gae=0;
+	priority_queue<pair<ll,bool>> q;//true먼저
+	for(int i=0;i<n;++i){
+		cin>>a>>b>>c;//끝나는건 true,시작은 false
+		q.push({-b,false});
+		q.push({-c,true});
+	}
+	while(!q.empty()){
+		ll f=-q.top().first;
+		bool state=q.top().second;
+		q.pop();
+		if(state) --gae;
+		else ++gae;
+		maxGae=max(maxGae,gae);
+	}
+	cout<<maxGae;
+}
