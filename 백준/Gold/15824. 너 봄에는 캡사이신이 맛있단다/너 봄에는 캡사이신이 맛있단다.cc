98f2f1a40ec51d315c48749afe71ee4b3e2ba47f
ash9river <121378532+ash9river@users.noreply.github.com>
2024-05-09 09:56:41 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 64 ms, Memory: 6712 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.cc
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.cc
@@ -9,8 +9,13 @@ ll makeTable(ll n){
     ll& ret=table[n];
     if(ret!=0) return ret;
     if(n==0) return ret=1;
-    if(n&(1<<0)) return ret=(2*makeTable((n-1)/2)*makeTable((n-1)/2))%MOD;
-    return ret=(makeTable(n/2)*makeTable(n/2))%MOD;
+    if(n&(1<<0)) {
+        ll tmp=makeTable((n-1)/2);
+
+        return ret=(2*tmp*tmp)%MOD;
+    }
+    ll tmp=makeTable(n/2);
+    return ret=(tmp*tmp)%MOD;
 }
 int main(){
     cin.tie(0);
@@ -22,20 +27,14 @@ int main(){
         cin>>v[i];
     }
     sort(v.begin(),v.end());
+    makeTable(n);
     ll ans=0;
-    for(int i=0;i<n-1;++i){
-        for(int j=i+1;j<n;++j){
-            ll p=j-i-1;
-            ll val=v[j]-v[i];
-            if(p>0) {
-                ans+=val*makeTable(p);
-                ans=ans%MOD;
-            }
-            else {
-                ans+=val;
-                ans=ans%MOD;
-            }
-        }
+    // (v[i]-v[j])*2^x
+    // --> v[i]*2^x - v[j]*2^x
+    for(int i=0;i<n;++i){
+        ll fi=(v[i]*makeTable(i))%MOD;
+        ll se=(v[n-1-i]*makeTable(i))%MOD;
+        ans=(ans+fi-se+MOD)%MOD;
     }
     cout<<ans;
 }
